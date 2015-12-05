b9b1d201206fb00eca3fd5b17a576a42db3ad944
pbl <philip.burenstam.linder@gmail.com>
2015-12-05 15:05:22 +0100

    job create now allow address param

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -66,6 +66,6 @@ class JobsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_params
-      params.require(:job).permit(:max_rate, :description, :job_date, :performed, :owner_user_id)
+      params.require(:job).permit(:max_rate, :description, :job_date, :performed, :owner_user_id, :address)
     end
 end
