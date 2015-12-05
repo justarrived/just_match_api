5e3bf4c13ffbc5c7e4636a300da39801dc9f76fb
Jacob Burenstam <burenstam@gmail.com>
2015-12-05 14:00:58 +0100

    Assign skills on User create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      @user.skills = Skill.where(id: params[:user][:skills])
       render json: @user, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
