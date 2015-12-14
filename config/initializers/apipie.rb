88459f95d6490bb076fd1cf0c65d87b17b8dfa3e
Jacob Burenstam <burenstam@gmail.com>
2015-12-14 22:17:15 +0100

    Rename app from JustArrived => JustMatch

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,5 +1,5 @@
 Apipie.configure do |config|
-  config.app_name                = 'JustArrived'
+  config.app_name                = 'JustMatch API'
   config.api_base_url            = '/api'
   config.doc_base_url            = '/api_docs'
   config.reload_controllers      = Rails.env.development?
@@ -8,7 +8,7 @@ Apipie.configure do |config|
   config.markup                  = Apipie::Markup::Markdown.new
   config.api_routes              = Rails.application.routes
   config.app_info['1.0'] = '
-    Welcome to the JustArrived API v1. The API is everything you need for building a
+    Welcome to the JustMatch API v1. The API is everything you need for building a
     well functioning and nice client.
   '
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_just_arrived_session'
+Rails.application.config.session_store :cookie_store, key: '_just_match_session'
