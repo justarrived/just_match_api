b98816ed00ca57da7d617a35de51f043aec0e869
Jacob Burenstam <burenstam@gmail.com>
2016-01-02 11:07:04 +0100

    Add another chat test

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -11,8 +11,8 @@ class Chat < ActiveRecord::Base
       if users.length >= MIN_USERS && users.length <= MAX_USERS
         create!.tap { |chat| chat.users = users }
       else
-        # TODO: This isn't great since this error is lost when calling #valid?,
-        #       #validate, #save or #save!
+        # TODO: This isn't great since this error is lost when calling
+        #       #valid?, #validate, #save or #save! (extremely implicit behavior)
         #       This method should be extracted to its own class/service
         new.tap do |chat|
           chat.errors.add(:users, "must be between #{MIN_USERS}-#{MAX_USERS}")
