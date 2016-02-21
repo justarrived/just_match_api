27f56d05607f79c440fc62b2416dc5cb13203460
Jacob Burenstam <burenstam@gmail.com>
2016-02-21 14:13:32 +0100

    Add some simple tests for Administrate dashboards

---
+++ spec/dashboards/dashboard_manifest_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe DashboardManifest do
+  subject { described_class }
+
+  it 'has DASHBOARDS constant' do
+    expect(subject::DASHBOARDS).not_to be_nil
+  end
+
+  it 'has correct ROOT_DASHBOARD constant' do
+    expect(subject::ROOT_DASHBOARD).to eq(:jobs)
+  end
+end
---
+++ spec/dashboards/dashboard_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+[
+  ChatDashboard, ChatUserDashboard, CommentDashboard, JobDashboard, JobSkillDashboard,
+  JobUserDashboard, LanguageDashboard, MessageDashboard, SkillDashboard, UserDashboard,
+  UserLanguageDashboard, UserSkillDashboard
+].each do |dashboard_klass|
+  RSpec.describe dashboard_klass do
+    subject { described_class }
+
+    it 'has ATTRIBUTE_TYPES constant' do
+      expect(subject::ATTRIBUTE_TYPES).not_to be_nil
+    end
+
+    it 'has COLLECTION_ATTRIBUTES constant' do
+      expect(subject::COLLECTION_ATTRIBUTES).not_to be_nil
+    end
+
+    it 'has SHOW_PAGE_ATTRIBUTES constant' do
+      expect(subject::SHOW_PAGE_ATTRIBUTES).not_to be_nil
+    end
+
+    it 'has FORM_ATTRIBUTES constant' do
+      expect(subject::FORM_ATTRIBUTES).not_to be_nil
+    end
+  end
+end
