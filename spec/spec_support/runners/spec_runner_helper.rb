3fd538ca6ea972d66ce5ec8602142a373f01b884
Jacob Burenstam <burenstam@gmail.com>
2016-02-27 21:17:55 +0100

    Be smarter about when to run the test runners

--- spec/spec_support/runners/spec_runner_helper.rb
+++ spec/spec_support/runners/spec_runner_helper.rb
@@ -3,6 +3,6 @@ module SpecRunnerHelper
   def execute_runner?(env_name)
     # Only return true if running the entire test suite or if env_name is
     # explicitly set
-    !ARGV.first || ENV.fetch(env_name, false)
+    !ARGV.include?('spec') || ENV.fetch(env_name, false) == 'true'
   end
 end
