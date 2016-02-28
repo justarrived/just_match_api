29727c033025934b320746506e2cc9dbbc64ecdc
Jacob Burenstam <burenstam@gmail.com>
2016-02-28 11:39:20 +0100

    Add immigrant gem and add CheckDBIndexesRunner
    * Validate that now missing DB indexes exist
    * Easy generation of migration file through rake task

---
+++ spec/spec_support/runners/check_db_indexes_runner.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+require 'spec_support/runners/spec_runner_helper'
+
+module CheckDBIndexesRunner
+  extend SpecRunnerHelper
+
+  def self.run
+    return unless execute_runner?('CHECK_DB_INDEXES')
+
+    print 'Checking for missing DB indexes..'
+
+    Rails.application.eager_load!
+
+    keys, warnings = Immigrant::KeyFinder.new.infer_keys
+
+    puts if keys.any? || warnings.values.any?
+
+    warnings.values.each { |warning| $stderr.puts "WARNING: #{warning}" }
+
+    keys.each do |key|
+      column = key.options[:column]
+      pk = key.options[:primary_key]
+      from = "#{key.from_table}.#{column}"
+      to = "#{key.to_table}.#{pk}"
+      $stderr.puts "Missing foreign key relationship on '#{from}' to '#{to}'"
+    end
+
+    if keys.any?
+      puts 'Found missing foreign keys:'
+      puts [
+        'Please run `rails generate immigration AddMissingKeys`',
+        'to create a migration to add them.'
+      ].join(' ')
+      puts 'CheckDBIndexes failed!'
+      exit keys.count
+    end
+
+    print "done \n"
+  end
+end
