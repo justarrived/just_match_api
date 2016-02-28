5d8af753800d8a97f55eb915c4efaeb5891b557c
Jacob Burenstam <burenstam@gmail.com>
2016-02-28 11:00:15 +0100

    Fixed bug in SpecRunnerHelper#execute_runner?

--- spec/spec_support/runners/spec_runner_helper.rb
+++ spec/spec_support/runners/spec_runner_helper.rb
@@ -3,6 +3,7 @@ module SpecRunnerHelper
   def execute_runner?(env_name)
     # Only return true if running the entire test suite or if env_name is
     # explicitly set
-    !ARGV.include?('spec') || ENV.fetch(env_name, false) == 'true'
+    filtered_test = ARGV.map { |arg| arg.starts_with?('spec') }.include?(true)
+    !filtered_test || ENV.fetch(env_name, false) == 'true'
   end
 end
