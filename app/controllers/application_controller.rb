caf6bce2de4f5125739b574b16dd2a35f0c99f69
Jacob Burenstam <burenstam@gmail.com>
2016-03-06 10:33:50 +0100

    Protect from forgery with exception in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@
 class ApplicationController < ActionController::Base
   include HttpBasicAdminAuthenticator
 
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
 end
