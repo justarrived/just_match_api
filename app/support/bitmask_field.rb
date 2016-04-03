9c873f9271ad85c40a5cb0562e64cc034c7670d9
Jacob Burenstam <burenstam@gmail.com>
2016-04-03 22:39:19 +0200

    Add BitmaskField class, transform bitmask to array

---
+++ app/support/bitmask_field.rb
@@ -0,0 +1,9 @@
+class BitmaskField
+  def self.to_mask(new_array, array)
+    (new_array & array).map { |r| 2 ** array.index(r) }.sum
+  end
+
+  def self.from_mask(mask, array)
+    array.reject { |r| ((mask || 0) & 2 ** array.index(r)).zero? }
+  end
+end
---
+++ spec/support/bitmask_field_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe BitmaskField do
+  describe '#to_a' do
+    let(:array) { %w(value watman whatever) }
+    let(:other_array) { %w(watman) }
+    let(:another_array) { %w(watman whatever) }
+
+    describe '#to_mask' do
+      it 'can convert array to bitmask' do
+        expect(described_class.to_mask(other_array, array)).to eq(2)
+        expect(described_class.to_mask(another_array, array)).to eq(6)
+      end
+    end
+
+    describe '#from_mask' do
+      it 'can convert from bitmask to array' do
+        expect(described_class.from_mask(2, array)).to eq(other_array)
+        expect(described_class.from_mask(6, array)).to eq(another_array)
+      end
+    end
+  end
+end
