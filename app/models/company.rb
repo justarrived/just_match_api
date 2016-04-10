48059ddfc616104b3152f1bcee817b67100be22f
Jacob Burenstam <burenstam@gmail.com>
2016-04-10 22:27:17 +0200

    Allow frilans_finans_id to be nil for User and Company

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,7 +5,7 @@ class Company < ApplicationRecord
 
   validates :name, length: { minimum: 2 }, allow_blank: false
   validates :cin, uniqueness: true, length: { is: 10 }, allow_blank: false
-  validates :frilans_finans_id, uniqueness: true
+  validates :frilans_finans_id, uniqueness: true, allow_nil: true
 end
 
 # == Schema Information
--- db/migrate/20160410201516_add_frilans_finans_id_to_companies.rb
+++ db/migrate/20160410201516_add_frilans_finans_id_to_companies.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class AddFrilansFinansIdToCompanies < ActiveRecord::Migration
   def change
     add_column :companies, :frilans_finans_id, :integer
