9e3c492c4c57415f98f17c085a6450297d48966c
Jacob Burenstam <burenstam@gmail.com>
2016-04-11 22:21:15 +0200

    Set locale in test environment

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -120,20 +120,15 @@ module Api
 
       def set_locale
         locale_header = api_locale_header
-        if locale_header
-          # Only allow available locales
-          I18n.available_locales.map(&:to_s).each do |locale|
-            I18n.locale = locale if locale == locale_header
-          end
+        if locale_header.nil?
+          I18n.locale = current_user.locale
           return
         end
 
-        # NOTE: There is probably a way to avoid this, but currently in tests we must
-        # allow invalid locales and therefore we can't set the locale here, since it will
-        # cause translations missing errors & no such locale errors
-        return if Rails.env.test?
-
-        I18n.locale = current_user.locale
+        # Only allow available locales
+        I18n.available_locales.map(&:to_s).each do |locale|
+          I18n.locale = locale if locale == locale_header
+        end
       end
 
       def api_locale_header
