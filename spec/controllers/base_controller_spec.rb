3cf6f122cf08a02c102791d95c94229457bf9891
Jacob Burenstam <burenstam@gmail.com>
2016-04-12 18:22:52 +0200

    Reset current locale to default locale after each test

--- spec/controllers/base_controller_spec.rb
+++ spec/controllers/base_controller_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 
 # Even though this describes SkillsController, it should really be BaseController tests
 RSpec.describe Api::V1::SkillsController, type: :controller do
-  it 'can set the locale from header' do
-    request.headers['X-API-LOCALE'] = 'sv'
-    get :index, {}, {}
-    expect(I18n.locale).to eq(:sv)
+  context 'locale' do
+    after(:each) do
+      I18n.locale = I18n.default_locale
+    end
+
+    it 'can set the locale from header' do
+      request.headers['X-API-LOCALE'] = 'sv'
+      get :index, {}, {}
+      expect(I18n.locale).to eq(:sv)
+    end
   end
 end
