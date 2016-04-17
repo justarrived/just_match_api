7281de5e2bb974022be07828492c99c1e9ea4c46
Jacob Burenstam <burenstam@gmail.com>
2016-04-17 15:40:57 +0200

    Add test for FrilansFinansImporter#currencies

--- spec/support/frilans_finans_importer_spec.rb
+++ spec/support/frilans_finans_importer_spec.rb
@@ -4,16 +4,33 @@ require 'rails_helper'
 RSpec.describe FrilansFinansImporter do
   subject { described_class }
 
-  let(:professions_mock) do
-    resource = OpenStruct.new(attributes: { 'title' => 'watman' }, id: 1)
-    Struct.new(:resources).new([resource])
+  describe '#professions' do
+    let(:professions_mock) do
+      resource = OpenStruct.new(attributes: { 'title' => 'watman' }, id: 1)
+      Struct.new(:resources).new([resource])
+    end
+
+    it 'can import professions' do
+      allow(FrilansFinansApi::Profession).to receive(:walk).and_yield(professions_mock)
+
+      expect do
+        described_class.professions
+      end.to change(Category, :count).by(1)
+    end
   end
 
-  it 'can import professions' do
-    allow(FrilansFinansApi::Profession).to receive(:walk).and_yield(professions_mock)
+  describe '#currencies' do
+    let(:currencies_mock) do
+      resource = OpenStruct.new(attributes: { 'currency_code' => 'SEK' }, id: 1)
+      Struct.new(:resources).new([resource])
+    end
 
-    expect do
-      described_class.professions
-    end.to change(Category, :count).by(1)
+    it 'can import currencies' do
+      allow(FrilansFinansApi::Currency).to receive(:walk).and_yield(currencies_mock)
+
+      expect do
+        described_class.currencies
+      end.to change(Currency, :count).by(1)
+    end
   end
 end
