e7eeb03192e6658abeef5f4570266f28ee92e189
Jacob Burenstam <burenstam@gmail.com>
2016-04-20 09:25:26 +0200

    Add AWS_REGION env variable and define region in paperclip config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
+      s3_region: ENV['AWS_REGION'],
       bucket: ENV['S3_BUCKET_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
