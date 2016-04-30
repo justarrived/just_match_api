4a6f65048fd2b526984713053e231ba16034d0e5
Jacob Burenstam <burenstam@gmail.com>
2016-04-30 13:45:46 +0200

    Add ability to force Frilans Finans live seed

--- config/initializers/frilans_finans_api.rb
+++ config/initializers/frilans_finans_api.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-FrilansFinansApi.client_klass = FrilansFinansApi::Client
+FrilansFinansApi.client_klass = FrilansFinansApi::NilClient
 FrilansFinansApi.base_uri = ENV.fetch('FRILANS_FINANS_BASE_URI')
 FrilansFinansApi.client_id = ENV.fetch('FRILANS_FINANS_CLIENT_ID')
 FrilansFinansApi.client_secret = ENV.fetch('FRILANS_FINANS_CLIENT_SECRET')
