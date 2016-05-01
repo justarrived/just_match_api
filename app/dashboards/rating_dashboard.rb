f16f9994b9160f6b9b5b4a2b161bf078e704654f
Jacob Burenstam <burenstam@gmail.com>
2016-05-01 23:06:43 +0200

    Add score to admin rating collection view

--- app/dashboards/rating_dashboard.rb
+++ app/dashboards/rating_dashboard.rb
@@ -24,6 +24,7 @@ class RatingDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
+    :score,
     :from_user,
     :to_user,
     :job
