3a9b2b63ebbfd0423ae8dff686c9c453aa3348e0
Jacob Burenstam <burenstam@gmail.com>
2016-05-08 14:03:17 +0200

    Wrap Frilans Finans company attributes under company key

--- app/models/frilans_finans/company_wrapper.rb
+++ app/models/frilans_finans/company_wrapper.rb
@@ -3,13 +3,15 @@ module FrilansFinans
   module CompanyWrapper
     def self.attributes(company:, user:)
       {
-        name: company.name,
-        country: company.country_name.upcase,
-        street: company.street,
-        city: company.city,
-        zip: company.zip,
-        send_to_email: company.email,
-        user_id: user.frilans_finans_id
+        company: {
+          name: company.name,
+          country: company.country_name.upcase,
+          street: company.street,
+          city: company.city,
+          zip: company.zip,
+          send_to_email: company.email,
+          user_id: user.frilans_finans_id
+        }
       }
     end
   end
--- spec/models/frilans_finans/company_wrapper_spec.rb
+++ spec/models/frilans_finans/company_wrapper_spec.rb
@@ -8,13 +8,15 @@ RSpec.describe FrilansFinans::CompanyWrapper do
       company = FactoryGirl.build(:company)
       result = described_class.attributes(company: company, user: user)
       expected = {
-        name: company.name,
-        country: company.country_name.upcase,
-        street: company.street,
-        city: company.city,
-        zip: company.zip,
-        send_to_email: company.email,
-        user_id: user.frilans_finans_id
+        company: {
+          name: company.name,
+          country: company.country_name.upcase,
+          street: company.street,
+          city: company.city,
+          zip: company.zip,
+          send_to_email: company.email,
+          user_id: user.frilans_finans_id
+        }
       }
       expect(result).to eq(expected)
     end
