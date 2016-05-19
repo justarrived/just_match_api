b31017b619c5777ef12b91d8476e876fc1e56f1a
Jacob Burenstam <burenstam@gmail.com>
2016-05-19 14:02:03 +0200

    Allow inclusion of user language(s) in job user endpoint

--- app/controllers/api/v1/jobs/job_users_controller.rb
+++ app/controllers/api/v1/jobs/job_users_controller.rb
@@ -30,7 +30,7 @@ module Api
           api_versions '1.0'
         end
 
-        ALLOWED_INCLUDES = %w(job user user.user_images).freeze
+        ALLOWED_INCLUDES = %w(job user user.user_images user.language user.languages).freeze # rubocop:disable Metrics/LineLength
 
         api :GET, '/jobs/:job_id/users', 'Show job users'
         description 'Returns list of job users if the user is allowed.'
