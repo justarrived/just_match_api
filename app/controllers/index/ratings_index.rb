795035d9c9463b70ebd40cdacfe1fdbd5772d6c4
Jacob Burenstam <burenstam@gmail.com>
2016-05-20 09:48:24 +0200

    Allow to filter Ratings on job id and score

--- app/controllers/index/ratings_index.rb
+++ app/controllers/index/ratings_index.rb
@@ -2,6 +2,7 @@
 module Index
   class RatingsIndex < BaseIndex
     SORTABLE_FIELDS = %i(score created_at).freeze
+    ALLOWED_FILTERS = %i(job_id score).freeze
 
     def ratings(scope = Rating)
       @ratings ||= prepare_records(scope)
