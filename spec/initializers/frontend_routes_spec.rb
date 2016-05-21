f552b3b256e6a0149cbd4ed65ff7d4d31e80ca45
Jacob Burenstam <burenstam@gmail.com>
2016-05-21 17:40:31 +0200

    Update Just Arrived frontend app URL

--- spec/initializers/frontend_routes_spec.rb
+++ spec/initializers/frontend_routes_spec.rb
@@ -3,53 +3,54 @@ require 'rails_helper'
 
 RSpec.describe FrontendRoutesReader do
   subject { described_class.new }
+  let(:base_url) { 'https://app.justarrived.se/' }
 
   it 'can read routes file' do
     expect(subject.routes).to be_a(Hash)
   end
 
   it 'has base url' do
-    expect(subject.base_url).to include('https://justarrived.se/')
+    expect(subject.base_url).to include(base_url)
   end
 
   it 'returns route with base url' do
-    expect(subject.draw(:login)).to include('https://justarrived.se/')
+    expect(subject.draw(:login)).to include(base_url)
   end
 
   it 'returns login route' do
     result = subject.draw(:login)
-    expected = 'https://justarrived.se/user/signin'
+    expected = "#{base_url}user/signin"
     expect(result).to eq(expected)
   end
 
   it 'returns faqs route' do
     result = subject.draw(:faqs)
-    expected = 'https://justarrived.se/faq'
+    expected = "#{base_url}faq"
     expect(result).to eq(expected)
   end
 
   it 'returns route for a single job' do
     result = subject.draw(:job, id: 1)
-    expected = 'https://justarrived.se/jobs/1'
+    expected = "#{base_url}jobs/1"
     expect(result).to eq(expected)
   end
 
   it 'returns route for jobs' do
     result = subject.draw(:jobs)
-    expected = 'https://justarrived.se/jobs'
+    expected = "#{base_url}jobs"
     expect(result).to eq(expected)
   end
 
   it 'returns route for a job user' do
     result = subject.draw(:job_user, id: 1)
-    expected = 'https://justarrived.se/arriver/1'
+    expected = "#{base_url}arriver/1"
     expect(result).to eq(expected)
   end
 
   it 'returns reset password route' do
     token = 'asd'
     result = subject.draw(:reset_password, token: token)
-    expected = "https://justarrived.se/reset_password/#{token}"
+    expected = "#{base_url}reset_password/#{token}"
     expect(result).to eq(expected)
   end
 end
