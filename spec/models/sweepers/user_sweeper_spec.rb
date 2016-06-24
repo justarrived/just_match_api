b397f94570bf48374c9969262596a1e4a93f3f49
Jacob Burenstam <burenstam@gmail.com>
2016-06-24 11:46:47 +0200

    Don't create Frilans Finans user for anonymized users
    Closes #444

--- spec/models/sweepers/user_sweeper_spec.rb
+++ spec/models/sweepers/user_sweeper_spec.rb
@@ -3,23 +3,37 @@ require 'rails_helper'
 
 RSpec.describe Sweepers::UserSweeper do
   describe '#create_frilans_finans' do
-    it 'sets user frilans finans id' do
-      user = FactoryGirl.create(:user, frilans_finans_id: nil)
+    context 'active Frilans Finans client' do
+      it 'sets user frilans finans id' do
+        user = FactoryGirl.create(:user, frilans_finans_id: nil)
 
-      isolate_frilans_finans_client(FrilansFinansApi::FixtureClient) do
-        described_class.create_frilans_finans
-        user.reload
-        expect(user.frilans_finans_id).to eq(1)
+        isolate_frilans_finans_client(FrilansFinansApi::FixtureClient) do
+          described_class.create_frilans_finans
+          user.reload
+          expect(user.frilans_finans_id).to eq(1)
+        end
+      end
+
+      it 'does nothing with anonymized users' do
+        user = FactoryGirl.create(:user, frilans_finans_id: nil, anonymized: true)
+
+        isolate_frilans_finans_client(FrilansFinansApi::FixtureClient) do
+          described_class.create_frilans_finans
+          user.reload
+          expect(user.frilans_finans_id).to be_nil
+        end
       end
     end
 
-    it 'does nothing when frilans finans returns nil' do
-      user = FactoryGirl.create(:user, frilans_finans_id: nil)
+    context 'Frilans Finans nil client' do
+      it 'does nothing when frilans finans returns nil' do
+        user = FactoryGirl.create(:user, frilans_finans_id: nil)
 
-      isolate_frilans_finans_client(FrilansFinansApi::NilClient) do
-        described_class.create_frilans_finans
-        user.reload
-        expect(user.frilans_finans_id).to be_nil
+        isolate_frilans_finans_client(FrilansFinansApi::NilClient) do
+          described_class.create_frilans_finans
+          user.reload
+          expect(user.frilans_finans_id).to be_nil
+        end
       end
     end
   end
