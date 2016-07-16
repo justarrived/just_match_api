026212fbcd0109c2fee3bacd4fe3f525914d5304
Jacob Burenstam <burenstam@gmail.com>
2016-07-16 17:33:11 +0200

    Add FrilansFinansTerm and TermsAgreement dev seeds

---
+++ lib/seeds/dev/frilans_finans_term_seed.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+require 'seeds/base_seed'
+
+module Dev
+  class FrilansFinansTermSeed < BaseSeed
+    def self.call
+      max_ff_terms = max_count_opt('MAX_FF_TERMS_SEED', 3)
+
+      log_seed(FrilansFinansTerm) do
+        max_ff_terms.times do |n|
+          FrilansFinansTerm.create(company: true, body: Faker::Lorem.paragraph(2))
+          FrilansFinansTerm.create(company: false, body: Faker::Lorem.paragraph(2))
+        end
+      end
+    end
+  end
+end
