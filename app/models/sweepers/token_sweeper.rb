964f6391cc42c786c5f1ada0fd37c7c70538a1fd
Jacob Burenstam <burenstam@gmail.com>
2016-07-16 17:09:15 +0200

    TokenSweeper :lipstick:

--- app/models/sweepers/token_sweeper.rb
+++ app/models/sweepers/token_sweeper.rb
@@ -2,9 +2,7 @@
 module Sweepers
   class TokenSweeper
     def self.destroy_expired_tokens(scope = Token)
-      scope.expired.find_each(batch_size: 1000) do |token|
-        token.destroy!
-      end
+      scope.expired.find_each(batch_size: 1000, &:destroy!)
     end
   end
 end
