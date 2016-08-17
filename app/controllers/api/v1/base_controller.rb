2ea09d6283f63fc72851b00cee9a65ee784100d2
Jacob Burenstam <burenstam@gmail.com>
2016-08-17 15:43:24 +0200

    Fix the SSN format sent to Frilans Finans API
    Closes #506

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -4,7 +4,6 @@ module Api
     class BaseController < ::Api::BaseController
       resource_description do
         api_version '1.0'
-        # rubocop:disable Metrics/LineLength
         app_info <<-DOCDESCRIPTION
           # JustMatch API - v1.0 (beta) <a href="http://jsonapi.org/"><svg xmlns="http://www.w3.org/2000/svg" style="font-weight:normal;" width="90" height="20"><linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><mask id="a"><rect width="90" height="20" rx="3" fill="#fff"/></mask><g mask="url(#a)"><path fill="#555" d="M0 0h63v20H0z"/><path fill="#9f9f9f" d="M63 0h27v20H63z"/><path fill="url(#b)" d="M0 0h90v20H0z"/></g><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="31.5" y="15" fill="#010101" fill-opacity=".3">JSON API</text><text x="31.5" y="14">JSON API</text><text x="75.5" y="15" fill="#010101" fill-opacity=".3">1.0</text><text x="75.5" y="14">1.0</text></g></svg></a>
 
