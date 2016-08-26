105a76130c0a7ec288a61f723e708270548178c1
Jacob Burenstam <burenstam@gmail.com>
2016-08-26 17:07:17 +0200

    Return User statuses in all supported languages

--- app/support/user_statuses_serializer.rb
+++ app/support/user_statuses_serializer.rb
@@ -3,10 +3,14 @@
 class UserStatusesSerializer
   def self.serializeble_resource
     statuses_data = User::STATUSES.map do |status_name, status_id|
-      attributes = {
-        name: I18n.t("user.statuses.#{status_name}"),
-        description: I18n.t("user.statuses.#{status_name}_description")
-      }
+      attributes = {}
+      I18n.available_locales.each do |locale|
+        name = I18n.t("user.statuses.#{status_name}", locale: locale)
+        description = I18n.t("user.statuses.#{status_name}_description", locale: locale)
+
+        attributes[:"#{locale}_name"] = name
+        attributes[:"#{locale}_description"] = description
+      end
       JsonApiData.new(id: status_id, type: :'user-statuses', attributes: attributes)
     end
     JsonApiDatum.new(statuses_data)
--- spec/support/user_statuses_serializer_spec.rb
+++ spec/support/user_statuses_serializer_spec.rb
@@ -9,7 +9,12 @@ RSpec.describe UserStatusesSerializer do
     it 'returns serialized statues' do
       data = subject[:data].first
       expect(data[:id]).to eq(1)
-      expect(data[:attributes][:name]).to eq(I18n.t('user.statuses.asylum_seeker'))
+
+      en_name = I18n.t('user.statuses.asylum_seeker', locale: :en)
+      ar_name = I18n.t('user.statuses.asylum_seeker', locale: :ar)
+
+      expect(data[:attributes][:'en-name']).to eq(en_name)
+      expect(data[:attributes][:'ar-name']).to eq(ar_name)
     end
   end
 end
