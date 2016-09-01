5437e2ad9221638c373796866f13d04bc5756c49
Jacob Burenstam <burenstam@gmail.com>
2016-09-01 12:19:12 +0200

    Use patched version of the personnummer gem to fix samordningsnummer normalization

--- spec/support/swedish_ssn_spec.rb
+++ spec/support/swedish_ssn_spec.rb
@@ -2,9 +2,18 @@
 require 'spec_helper'
 
 RSpec.describe SwedishSSN do
+  let(:personnummer) { '8908030334' }
+  let(:personnummer_normalized) { '890803-0334' }
+  let(:samordningsnummer) { '7010632391' }
+  let(:samordningsnummer_normalized) { '701063-2391' }
+
   describe '#valid?' do
     it 'returns true for a valid ssn' do
-      expect(described_class.valid?('890803-0334')).to eq(true)
+      expect(described_class.valid?(personnummer)).to eq(true)
+    end
+
+    it 'returns true for a valid "samordningsnummer"' do
+      expect(described_class.valid?(samordningsnummer)).to eq(true)
     end
 
     it 'returns false for a valid ssn' do
@@ -14,10 +23,14 @@ RSpec.describe SwedishSSN do
 
   describe '#normalize' do
     it 'returns the normalized ssn if valid' do
-      ssn = '890803-0334'
-      expect(described_class.normalize('198908030334')).to eq(ssn)
-      expect(described_class.normalize('19890803-0334')).to eq(ssn)
-      expect(described_class.normalize('8908030334')).to eq(ssn)
+      expect(described_class.normalize('198908030334')).to eq(personnummer_normalized)
+      expect(described_class.normalize('19890803-0334')).to eq(personnummer_normalized)
+      expect(described_class.normalize('8908030334')).to eq(personnummer_normalized)
+    end
+
+    it 'returns the normalized ssn if valid "samordningsnummer"' do
+      expected = samordningsnummer_normalized
+      expect(described_class.normalize(samordningsnummer)).to eq(expected)
     end
 
     it 'returns the ssn untouched if invalid' do
