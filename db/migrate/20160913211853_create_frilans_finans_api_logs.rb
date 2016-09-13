058d91f4f738e2c8338734468a582b662147fc3f
Jacob Burenstam <burenstam@gmail.com>
2016-09-14 00:46:52 +0200

    Add event logger capabilities to FrilansFinansApi

--- db/migrate/20160913211853_create_frilans_finans_api_logs.rb
+++ db/migrate/20160913211853_create_frilans_finans_api_logs.rb
@@ -3,6 +3,7 @@ class CreateFrilansFinansApiLogs < ActiveRecord::Migration
     create_table :frilans_finans_api_logs do |t|
       t.integer :status
       t.string :status_name
+      t.string :verb
       t.text :params
       t.text :response_body
       t.string :uri, limit: 2083
--- db/schema.rb
+++ db/schema.rb
@@ -159,6 +159,7 @@ ActiveRecord::Schema.define(version: 20160913211853) do
   create_table "frilans_finans_api_logs", force: :cascade do |t|
     t.integer  "status"
     t.string   "status_name"
+    t.string   "verb"
     t.text     "params"
     t.text     "response_body"
     t.string   "uri",           limit: 2083
