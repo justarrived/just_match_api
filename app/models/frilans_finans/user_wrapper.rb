f6fae37cd3677ddb47068a932449708fcbcf8006
Jacob Burenstam <burenstam@gmail.com>
2016-10-11 23:11:53 +0200

    Add support for no user ssn in FF user wrapper

--- app/models/frilans_finans/user_wrapper.rb
+++ app/models/frilans_finans/user_wrapper.rb
@@ -2,10 +2,8 @@
 module FrilansFinans
   module UserWrapper
     def self.attributes(user)
-      # Frilans Finans wants the social security number as an integer
-      # We store the ssn on the format YYMMDD-XXXX, so we need to remove '-'
-      ssn = format_ssn(user.ssn)
-      {
+      # NOTE: FFAPIv2: Wrap in data/attributes instead of user
+      attrs = {
         user: {
           email: user.email,
           street: user.street || '',
@@ -14,13 +12,20 @@ module FrilansFinans
           country: user.country_name.upcase,
           cellphone: user.phone,
           first_name: user.first_name,
-          last_name: user.last_name,
-          social_security_number: ssn
+          last_name: user.last_name
         }
       }
+
+      ssn = user.ssn
+      # Company users doesn't need to have ssn set
+      attrs[:user][:social_security_number] = format_ssn(ssn) unless ssn.nil?
+      attrs
     end
 
     def self.format_ssn(ssn)
+      # Frilans Finans wants the social security number as an integer
+      # We store the ssn on the format YYMMDD-XXXX, so we need to remove '-'
+      # NOTE: FFAPIv2: ssn should be a string!
       ssn.delete('-').to_i
     end
   end
