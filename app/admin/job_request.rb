a7bf143d66b96de622e9430460fa67a64b28c48f
Jacob Burenstam <burenstam@gmail.com>
2016-10-16 21:11:07 +0200

    Update job request admin dashboard. Closes #613

--- app/admin/job_request.rb
+++ app/admin/job_request.rb
@@ -1,5 +1,33 @@
 # frozen_string_literal: true
 ActiveAdmin.register JobRequest do
+  scope :all, default: true
+  scope :pending
+  scope :finished
+
+  filter :company_name
+  filter :contact_string
+  filter :assignment
+  filter :job_scope
+  filter :job_specification
+  filter :language_requirements
+  filter :job_at_date
+  filter :responsible
+  filter :suitable_candidates
+  filter :comment
+  filter :finished
+
+  index do
+    column :finished
+    column :company_name
+    column :responsible
+    column :contact_string
+    column :language_requirements
+    column :job_at_date
+    column :created_at
+
+    actions
+  end
+
   permit_params do
     [
       :company_name,
--- app/models/job_request.rb
+++ app/models/job_request.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 class JobRequest < ActiveRecord::Base
+  scope :finished, -> { where(finished: true) }
+  scope :pending, -> { where(finished: false) }
 end
 
 # == Schema Information
