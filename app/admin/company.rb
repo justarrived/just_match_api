49ac3f11dbbb83f7bc2c5b6286276e17d658e459
Jacob Burenstam <burenstam@gmail.com>
2016-10-16 21:31:59 +0200

    Improve company admin dashboard

--- app/admin/company.rb
+++ app/admin/company.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 ActiveAdmin.register Company do
+  scope :all, default: true
+  scope :needs_frilans_finans_id
+
+  filter :name
+  filter :email
+  filter :phone
+  filter :cin
+  filter :street
+  filter :city
+  filter :created_at
+
+  index do
+    column :name
+    column :cin
+    column :frilans_finans_id
+    column :email
+    column :phone
+    column :created_at
+  end
+
   permit_params do
     [
       :name,
