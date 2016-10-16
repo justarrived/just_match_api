5c7a485a78d3042c174deab2e44f5fae8a3e92f0
Jacob Burenstam <burenstam@gmail.com>
2016-10-16 21:58:10 +0200

    Improve Rating admin dashboard

--- app/admin/rating.rb
+++ app/admin/rating.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 ActiveAdmin.register Rating do
+  scope :all, default: true
+
+  filter :score
+  filter :from_user
+  filter :to_user
+  filter :job
+  filter :created_at
+
+  index do
+    column :id
+    column :score
+    column :job
+    column :from_user
+    column :to_user
+    column :comment
+    column :created_at
+  end
+
   permit_params do
     [
       :from_user_id,
