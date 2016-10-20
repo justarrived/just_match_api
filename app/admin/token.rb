99cd48a8eedfc5145bb6116d0e4024efcced5010
Jacob Burenstam <burenstam@gmail.com>
2016-10-20 23:27:24 +0200

    Update Token admin dashboard

--- app/admin/token.rb
+++ app/admin/token.rb
@@ -1,3 +1,13 @@
 # frozen_string_literal: true
 ActiveAdmin.register Token do
+  filter :user
+  filter :created_at
+
+  index do
+    column :id
+    column :user
+    column :created_at
+
+    actions
+  end
 end
