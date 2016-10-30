f5f941688ebffcdf66bd4ad5b678b03a72406f1f
Jacob Burenstam <burenstam@gmail.com>
2016-10-30 20:52:31 +0100

    Refactor GoogleTranslate to be stateless

--- lib/i18n/google_translate.rb
+++ lib/i18n/google_translate.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 require 'google/cloud/translate'
 
-class GoogleTranslate
-  def initialize(api_key: ENV.fetch('GOOGLE_TRANSLATE_API_KEY'))
-    @translator = Google::Cloud::Translate.new(key: api_key)
-  end
-
-  def translate(text, from:, to:)
-    translation = @translator.translate(text, from: from, to: to)
+module GoogleTranslate
+  def self.translate(text, from:, to:, api_key: ENV.fetch('GOOGLE_TRANSLATE_API_KEY'))
+    translator = Google::Cloud::Translate.new(key: api_key)
+    translation = translator.translate(text, from: from, to: to)
     translation.text
   end
-  alias_method :t, :translate
+
+  def self.t(*args)
+    translate(*args)
+  end
 end
--- spec/lib/i18n/google_translate_spec.rb
+++ spec/lib/i18n/google_translate_spec.rb
@@ -11,19 +11,19 @@ RSpec.describe GoogleTranslate do
     allow_any_instance_of(Google::Cloud::Translate::Api).to(
       receive(:translate).and_return(google_translation_result_mock)
     )
-    described_class.new(api_key: 'xxx')
+    described_class
   end
 
   describe '#translate' do
     it 'translanslates the given text' do
-      result = subject.translate(text, from: :en, to: :sv)
+      result = subject.translate(text, from: :en, to: :sv, api_key: 'xxx')
       expect(result).to eq(translated_text)
     end
   end
 
   describe '#t' do
     it 'produces the same result as #translate' do
-      translate_result = subject.translate(text, from: :en, to: :sv)
+      translate_result = subject.translate(text, from: :en, to: :sv, api_key: 'xxx')
       t_result = subject.t(text, from: :en, to: :sv)
       expect(t_result).to eq(translate_result)
     end
