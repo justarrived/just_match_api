f5aba57c64573d365216ebec5f1b27c5c5bce5d2
Jacob Burenstam <burenstam@gmail.com>
2016-11-06 14:16:46 +0100

    Add MachineTranslationsJob tests

--- app/jobs/machine_translations_job.rb
+++ app/jobs/machine_translations_job.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-class MachineTranslationJob < ApplicationJob
+class MachineTranslationsJob < ApplicationJob
   def perform(translation)
     CreateMachineTranslationsService.call(translation: translation)
   end
