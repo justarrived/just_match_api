814fad669b19f58e649e6b01bf4ef4a67e642388
Jacob Burenstam <burenstam@gmail.com>
2016-11-11 00:25:19 +0100

    Fix resource sort order on admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -7,7 +7,7 @@ ActiveAdmin.register_page 'Dashboard' do
       column do
         panel 'Recent Candidates' do
           ul do
-            JobUser.last(20).map do |job_user|
+            JobUser.last(20).reverse.map do |job_user|
               li link_to(
                 "#{job_user.user.name} applied for #{job_user.job.name}",
                 admin_job_user_path(job_user)
@@ -20,7 +20,7 @@ ActiveAdmin.register_page 'Dashboard' do
       column do
         panel 'Recent Frilans Finans Invoices' do
           ul do
-            FrilansFinansInvoice.last(20).map do |ff_invoice|
+            FrilansFinansInvoice.last(20).reverse.map do |ff_invoice|
               li link_to(
                 "##{ff_invoice.name}", admin_frilans_finans_invoice_path(ff_invoice)
               )
@@ -32,7 +32,7 @@ ActiveAdmin.register_page 'Dashboard' do
       column do
         panel 'Recent Users' do
           ul do
-            User.last(20).map do |user|
+            User.last(20).reverse.map do |user|
               li link_to(user.name, admin_user_path(user))
             end
           end
@@ -42,7 +42,7 @@ ActiveAdmin.register_page 'Dashboard' do
       column do
         panel 'Recent Jobs' do
           ul do
-            Job.with_translations.last(20).map do |job|
+            Job.with_translations.last(20).reverse.map do |job|
               li link_to(job.original_name, admin_job_path(job))
             end
           end
