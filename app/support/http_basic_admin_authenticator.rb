221fa93f654331f8fe9d9b852443f81a89ee95f0
Jacob Burenstam <burenstam@gmail.com>
2016-12-02 16:53:57 +0100

    Add current admin user context to ActiveADmin

--- app/support/http_basic_admin_authenticator.rb
+++ app/support/http_basic_admin_authenticator.rb
@@ -5,7 +5,12 @@ module HttpBasicAdminAuthenticator
   def authenticate_admin
     authenticate_or_request_with_http_basic do |raw_email, password|
       email = raw_email&.strip&.downcase
-      User.admins.find_by_credentials(email_or_phone: email, password: password)
+      @authenticated_admin = User.admins.
+        find_by_credentials(email_or_phone: email, password: password)
     end
+  end
+
+  def authenticated_admin_user
+    @authenticated_admin
   end
 end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -87,7 +87,7 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # (within the application controller) to return the currently logged in user.
-  # config.current_user_method = :current_admin_user
+  config.current_user_method = :authenticated_admin_user
 
   # == Logging Out
   #
@@ -99,7 +99,8 @@ ActiveAdmin.setup do |config|
   # will call the method to return the path.
   #
   # Default:
-  config.logout_link_path = :destroy_admin_user_session_path
+  # config.logout_link_path = :destroy_admin_user_session_path
+  config.logout_link_path = 'http://justarrived.se'
 
   # This setting changes the http method used when rendering the
   # link. For example :get, :delete, :put, etc..
