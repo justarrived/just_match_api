005c226116404c59b416db6fd27db45983096372
Jacob Burenstam <burenstam@gmail.com>
2016-12-30 02:38:18 +0100

    Fix name error in BaseNotifier

--- app/notifiers/base_notifier.rb
+++ app/notifiers/base_notifier.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class BaseNotifier
   def self.ignored?(user)
-    user.ignored_notification?(underscored_name) || globally_ignored?(notification_name)
+    user.ignored_notification?(underscored_name) || globally_ignored?(underscored_name)
   end
 
   def self.globally_ignored?(notification_name)
