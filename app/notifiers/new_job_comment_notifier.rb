1c83e1b9e63b95fe4f020619ae310e4a862930c5
Jacob Burenstam <burenstam@gmail.com>
2016-12-30 03:06:09 +0100

    Don't send comment notification to the owner of the comment

--- app/notifiers/new_job_comment_notifier.rb
+++ app/notifiers/new_job_comment_notifier.rb
@@ -3,6 +3,7 @@ class NewJobCommentNotifier < BaseNotifier
   def self.call(comment:, job:)
     owner = job.owner
     return if ignored?(owner)
+    return if owner == comment.owner
 
     with_locale(owner.locale) do
       JobMailer.
