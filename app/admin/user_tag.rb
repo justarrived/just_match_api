3b3721b16135ac06b69aa0637f4ca3fc9a38d517
Jacob Burenstam <burenstam@gmail.com>
2017-01-02 18:49:07 +0100

    Admin: Update UserTag index view

--- app/admin/user_tag.rb
+++ app/admin/user_tag.rb
@@ -2,7 +2,26 @@
 ActiveAdmin.register UserTag do
   menu parent: 'Users'
 
+  index do
+    selectable_column
+
+    column :id
+    column :name do |user_tag|
+      url = admin_users_path + AdminHelpers::Link.query(:user_tags_tag_id, user_tag.tag.id) # rubocop:disable Metrics/LineLength
+      link_to(user_tag.tag.display_name, url)
+    end
+    column :created_at
+
+    actions
+  end
+
   permit_params do
     [:user_id, :tag_id]
+  end
+
+  controller do
+    def scoped_collection
+      super.includes(:tag)
+    end
   end
 end
