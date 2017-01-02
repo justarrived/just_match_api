bdefad66542a91a532fbd5f8bce4a21d55bba4f8
Jacob Burenstam <burenstam@gmail.com>
2017-01-02 20:08:13 +0100

    Admin: Custom tag index view

--- app/admin/tag.rb
+++ app/admin/tag.rb
@@ -2,6 +2,16 @@
 ActiveAdmin.register Tag do
   menu parent: 'Settings'
 
+  index do
+    column :name
+    column :color do |tag|
+      style = "background-color: #{tag.color}"
+      span(tag.color, class: 'user-badge-tag-link', style: style)
+    end
+
+    actions
+  end
+
   permit_params do
     [:name, :color]
   end
