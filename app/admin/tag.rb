a7ef2120a75272abb738a68040eb0b16644f6221
Jacob Burenstam <burenstam@gmail.com>
2017-01-02 16:39:05 +0100

    Admin: menu placement for tags and user tags

--- app/admin/tag.rb
+++ app/admin/tag.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 ActiveAdmin.register Tag do
+  menu parent: 'Settings'
+
   permit_params do
     [:name, :color]
   end
--- app/admin/user_tag.rb
+++ app/admin/user_tag.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 ActiveAdmin.register UserTag do
+  menu parent: 'Users'
+
   permit_params do
     [:user_id, :tag_id]
   end
