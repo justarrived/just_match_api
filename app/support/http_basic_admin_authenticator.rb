96f8db654eb7645468b60717969bcb1bf7c2db93
Jacob Burenstam <burenstam@gmail.com>
2017-01-08 20:01:39 +0100

    Explicitly set EmailAddress from root namespace.
    See https://stackoverflow.com/questions/29636334/a-copy-of-xxx-has-been-removed-from-the-module-tree-but-is-still-active

--- app/support/http_basic_admin_authenticator.rb
+++ app/support/http_basic_admin_authenticator.rb
@@ -4,7 +4,7 @@ module HttpBasicAdminAuthenticator
 
   def authenticate_admin
     authenticate_or_request_with_http_basic do |raw_email, password|
-      email = EmailAddress.normalize(raw_email)
+      email = ::EmailAddress.normalize(raw_email)
       @authenticated_admin = begin
         User.admins.find_by_credentials(email_or_phone: email, password: password)
       end
