f4831f09cc6c36d3e20a571a2a8140342c5f16ab
Jacob Burenstam <burenstam@gmail.com>
2017-01-09 22:02:23 +0100

    User status endpoint I18n
    Closes #785, Closes #784

--- app/support/user_statuses_serializer.rb
+++ app/support/user_statuses_serializer.rb
@@ -3,13 +3,15 @@ class UserStatusesSerializer
   def self.serializeble_resource(key_transform:)
     statuses_data = User::STATUSES.map do |status_name, _status_id|
       attributes = {}
-      I18n.available_locales.each do |locale|
-        name = I18n.t("user.statuses.#{status_name}", locale: locale)
-        description = I18n.t("user.statuses.#{status_name}_description", locale: locale)
+      attributes.merge!(deprecated_resource_values_hash(status_name))
 
-        attributes[:"#{locale}_name"] = name
-        attributes[:"#{locale}_description"] = description
-      end
+      locale = I18n.locale
+      name = I18n.t("user.statuses.#{status_name}", locale: locale)
+      description = I18n.t("user.statuses.#{status_name}_description", locale: locale)
+      attributes[:name] = name
+      attributes[:description] = description
+      attributes[:translated_text] = { name: name, description: description }
+
       JsonApiData.new(
         id: status_name,
         type: :user_statuses,
@@ -18,5 +20,19 @@ class UserStatusesSerializer
       )
     end
     JsonApiDatum.new(statuses_data)
+  end
+
+  def self.deprecated_resource_values_hash(status_name)
+    ActiveSupport::Deprecation.warn('This method is deprecated and will be removed.')
+
+    attributes = {}
+    I18n.available_locales.each do |locale|
+      name = I18n.t("user.statuses.#{status_name}", locale: locale)
+      description = I18n.t("user.statuses.#{status_name}_description", locale: locale)
+
+      attributes[:"#{locale}_name"] = name
+      attributes[:"#{locale}_description"] = description
+    end
+    attributes
   end
 end
