6cfaa5ad00447c6bfdf3aa0f897416a2368e562d
Jacob Burenstam <burenstam@gmail.com>
2017-01-10 18:15:20 +0100

    Add Job belongs to justarrived_contact (type: User) migration

--- spec/factories/job_factory.rb
+++ spec/factories/job_factory.rb
@@ -97,31 +97,33 @@ end
 #
 # Table name: jobs
 #
-#  id                :integer          not null, primary key
-#  description       :text
-#  job_date          :datetime
-#  hours             :float
-#  name              :string
-#  created_at        :datetime         not null
-#  updated_at        :datetime         not null
-#  owner_user_id     :integer
-#  latitude          :float
-#  longitude         :float
-#  language_id       :integer
-#  street            :string
-#  zip               :string
-#  zip_latitude      :float
-#  zip_longitude     :float
-#  hidden            :boolean          default(FALSE)
-#  category_id       :integer
-#  hourly_pay_id     :integer
-#  verified          :boolean          default(FALSE)
-#  job_end_date      :datetime
-#  cancelled         :boolean          default(FALSE)
-#  filled            :boolean          default(FALSE)
-#  short_description :string
-#  featured          :boolean          default(FALSE)
-#  upcoming          :boolean          default(FALSE)
+#  id                           :integer          not null, primary key
+#  description                  :text
+#  job_date                     :datetime
+#  hours                        :float
+#  name                         :string
+#  created_at                   :datetime         not null
+#  updated_at                   :datetime         not null
+#  owner_user_id                :integer
+#  latitude                     :float
+#  longitude                    :float
+#  language_id                  :integer
+#  street                       :string
+#  zip                          :string
+#  zip_latitude                 :float
+#  zip_longitude                :float
+#  hidden                       :boolean          default(FALSE)
+#  category_id                  :integer
+#  hourly_pay_id                :integer
+#  verified                     :boolean          default(FALSE)
+#  job_end_date                 :datetime
+#  cancelled                    :boolean          default(FALSE)
+#  filled                       :boolean          default(FALSE)
+#  short_description            :string
+#  featured                     :boolean          default(FALSE)
+#  upcoming                     :boolean          default(FALSE)
+#  company_contact_user_id      :integer
+#  just_arrived_contact_user_id :integer
 #
 # Indexes
 #
@@ -131,8 +133,10 @@ end
 #
 # Foreign Keys
 #
-#  fk_rails_1cf0b3b406    (category_id => categories.id)
-#  fk_rails_70cb33aa57    (language_id => languages.id)
-#  fk_rails_b144fc917d    (hourly_pay_id => hourly_pays.id)
-#  jobs_owner_user_id_fk  (owner_user_id => users.id)
+#  fk_rails_1cf0b3b406                   (category_id => categories.id)
+#  fk_rails_70cb33aa57                   (language_id => languages.id)
+#  fk_rails_b144fc917d                   (hourly_pay_id => hourly_pays.id)
+#  jobs_company_contact_user_id_fk       (company_contact_user_id => users.id)
+#  jobs_just_arrived_contact_user_id_fk  (just_arrived_contact_user_id => users.id)
+#  jobs_owner_user_id_fk                 (owner_user_id => users.id)
 #
