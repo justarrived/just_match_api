04a378ce73c3d697ab98168c6ec90575b9897469
Jacob Burenstam <burenstam@gmail.com>
2017-01-10 02:26:49 +0100

    API: Add filter by language name support for all locales

--- app/controllers/index/languages_index.rb
+++ app/controllers/index/languages_index.rb
@@ -2,12 +2,24 @@
 module Index
   class LanguagesIndex < BaseIndex
     MAX_PER_PAGE = 300
-    FILTER_MATCH_TYPES = { en_name: :starts_with }.freeze
+    FILTER_MATCH_TYPES = {
+      en_name: :starts_with,
+      sv_name: :starts_with,
+      ar_name: :starts_with,
+      fa_name: :starts_with,
+      fa_af_name: :starts_with,
+      ku_name: :starts_with,
+      ti_name: :starts_with,
+      ps_name: :starts_with
+    }.freeze
     SORTABLE_FIELDS = %i(
       created_at lang_code direction system_language en_name sv_name ar_name fa_name
       fa_af_name ku_name ti_name ps_name
     ).freeze
-    ALLOWED_FILTERS = %i(lang_code en_name direction system_language).freeze
+    ALLOWED_FILTERS = %i(
+      lang_code direction system_language
+      en_name sv_name ar_name fa_name fa_af_name ku_name ti_name ps_name
+    ).freeze
 
     def languages(scope = Language)
       @languages ||= prepare_records(scope)
