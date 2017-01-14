80d6cc223633c2a731dfcd4c9bebf26d8499f55d
Jacob Burenstam <burenstam@gmail.com>
2017-01-14 15:08:18 +0100

    Refactor BaseNotifier locale switch
    Closes #535

--- app/notifiers/base_notifier.rb
+++ app/notifiers/base_notifier.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class BaseNotifier
   def self.notify(user: nil, locale: I18n.locale)
-    with_locale(locale) { yield unless ignored?(user) }
+    I18n.with_locale(locale) { yield unless ignored?(user) }
     true
   rescue Redis::ConnectionError => e
     ErrorNotifier.send(e, context: { locale: locale })
@@ -20,12 +20,5 @@ class BaseNotifier
 
   def self.underscored_name
     to_s.chomp('Notifier').underscore
-  end
-
-  def self.with_locale(locale)
-    previous_locale = I18n.locale
-    I18n.locale = locale
-    yield
-    I18n.locale = previous_locale
   end
 end
