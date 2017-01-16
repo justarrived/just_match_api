67ca1f08f801c98f9998257c1ca084e87b83ea70
Jacob Burenstam <burenstam@gmail.com>
2017-01-16 15:32:26 +0100

    Revert "Refactor BaseNotifier locale switch"
    This reverts commit 80d6cc223633c2a731dfcd4c9bebf26d8499f55d.

--- app/notifiers/base_notifier.rb
+++ app/notifiers/base_notifier.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class BaseNotifier
   def self.notify(user: nil, locale: I18n.locale, name: nil)
-    I18n.with_locale(locale) { yield unless ignored?(user, name) }
+    with_locale(locale) { yield unless ignored?(user, name) }
     true
   rescue Redis::ConnectionError => e
     ErrorNotifier.send(e, context: { locale: locale })
@@ -20,5 +20,12 @@ class BaseNotifier
 
   def self.underscored_name
     to_s.chomp('Notifier').underscore
+  end
+
+  def self.with_locale(locale)
+    previous_locale = I18n.locale
+    I18n.locale = locale
+    yield
+    I18n.locale = previous_locale
   end
 end
