839171531476f1461577f338fd2eb81960104bb2
Jacob Burenstam <burenstam@gmail.com>
2017-01-17 15:26:46 +0100

    Hotfix: :hocho: method name typo

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -517,7 +517,7 @@ ActiveAdmin.register User do
       competence_text: permitted_params.dig(:user, :competence_text)
     }
     user.set_translation(translation_params)
-    SyncFrilansFinansUserJob.peform_later(user: user)
+    SyncFrilansFinansUserJob.perform_later(user: user)
   end
 
   permit_params do
