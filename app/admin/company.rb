5f25abbe086c22b324a143e6285eaafeba6254ee
Jacob Burenstam <burenstam@gmail.com>
2017-01-31 11:20:44 +0100

    Admin: Custom company show page

--- app/admin/company.rb
+++ app/admin/company.rb
@@ -28,6 +28,46 @@ ActiveAdmin.register Company do
     actions
   end
 
+  show do
+    attributes_table do
+      row :name
+      row :cin
+      row :billing_email
+      row :website
+      row :phone
+      row :email
+      row :street
+      row :zip
+      row :city
+      row :frilans_finans_id
+      row :created_at
+      row :updated_at
+    end
+
+    active_admin_comments
+  end
+
+  sidebar :relations, only: [:show, :edit] do
+    company_query = AdminHelpers::Link.query(:company_id, company.id)
+
+    ul do
+      li(
+        link_to(
+          I18n.t('admin.counts.owned_jobs', count: company.owned_jobs.count),
+          admin_jobs_path + company_query
+        )
+      )
+    end
+
+    hr
+
+    ul do
+      company.users.each do |user|
+        li link_to(user.display_name, admin_user_path(user))
+      end
+    end
+  end
+
   permit_params do
     [
       :name,
