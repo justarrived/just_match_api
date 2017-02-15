33f71568f4b7168e454df56f7d6a3eedde508eea
Jacob Burenstam <burenstam@gmail.com>
2017-02-15 16:32:12 +0100

    Remove commented include in ApplicationSerializer

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 class ApplicationSerializer < ActiveModel::Serializer
-  # include ActionView::Helpers::TextHelper # needed for #simple_format
-
   delegate :text_to_html, to: :value_formatter
 
   # Returns true if the resource is rendered as a collection
