822a8512d6f9179766ff53ed37e1e980c2a1a116
Jacob Burenstam <burenstam@gmail.com>
2017-02-19 03:15:36 +0100

    Admin: Improved UserLanguage index view

--- app/admin/user_language.rb
+++ app/admin/user_language.rb
@@ -2,7 +2,25 @@
 ActiveAdmin.register UserLanguage do
   menu parent: 'Users'
 
+  index do
+    selectable_column
+
+    column :id
+    column :user
+    column :language
+    column :proficiency do |user_language|
+      "#{user_language.proficiency || '-'}/#{user_language.proficiency_by_admin || '-'}"
+    end
+    column :updated_at
+  end
+
   permit_params do
     [:user_id, :language_id, :proficiency, :proficiency_by_admin]
+  end
+
+  controller do
+    def scoped_collection
+      super.includes(:user, :language)
+    end
   end
 end
