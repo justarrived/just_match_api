9862483a6ad388da6178ec85cb48b92626c779be
Jacob Burenstam <burenstam@gmail.com>
2017-03-26 18:24:00 +0200

    Refactor DetectLanguage and set min confidence level to 50%

--- lib/i18n/detect_language.rb
+++ lib/i18n/detect_language.rb
@@ -4,9 +4,11 @@ require 'i18n/google_translate'
 module DetectLanguage
   LanguageDetectionResult = Struct.new(:text, :confidence, :source, :source_override)
 
+  MIN_CONFIDENCE_LEVEL_FOR_TRANSLATIONS = 0.50
+
   def self.call(text)
     detection = GoogleTranslate.detect(text)
-    return if detection.confidence < 0.70 # TODO: Figure out a resonable value for this
+    return unless within_confidence_level?(detection.confidence)
 
     LanguageDetectionResult.new(
       text, detection.confidence, detection.language, source_override(detection.language)
@@ -16,5 +18,9 @@ module DetectLanguage
   def self.source_override(locale)
     return 'sv' if %w(no da).include?(locale.to_s)
     nil
+  end
+
+  def self.within_confidence_level?(confidence)
+    confidence > MIN_CONFIDENCE_LEVEL_FOR_TRANSLATIONS
   end
 end
