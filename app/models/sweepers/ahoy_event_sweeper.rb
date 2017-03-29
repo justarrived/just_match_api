66abf1f401c8bf85fc1e75a3e0f318da247b6de4
Jacob Burenstam <burenstam@gmail.com>
2017-03-29 18:44:58 +0200

    Add AhoyEvenetSweeper that can delete events older than X and add rake task
    Closes #999

---
+++ app/models/sweepers/ahoy_event_sweeper.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+module Sweepers
+  module AhoyEventSweeper
+    def self.destroy_old(before_date:, scope: Ahoy::Event)
+      scope.where('time < ?', before_date).
+        find_each(batch_size: 1000, &:destroy)
+    end
+  end
+end
