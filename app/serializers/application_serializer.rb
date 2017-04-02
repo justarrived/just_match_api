5c4903f5ece396cfd12fcb2f6866e1f6792d7326
Jacob Burenstam <burenstam@gmail.com>
2017-04-02 21:52:15 +0200

    Add BaseController#current_language and pass it to serializer scope

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -4,6 +4,18 @@ class ApplicationSerializer < ActiveModel::Serializer
   delegate :to_unit, to: :number_formatter
   delegate :to_delimited, to: :number_formatter
 
+  def current_user
+    scope[:current_user]
+  end
+
+  def current_language
+    scope[:current_language]
+  end
+
+  def current_language_id
+    scope[:current_language_id]
+  end
+
   # Returns true if the resource is rendered as a collection
   def collection_serializer?
     !instance_options[:each_serializer].nil?
