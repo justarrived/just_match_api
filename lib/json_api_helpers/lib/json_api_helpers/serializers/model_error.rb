8817504194f418889a9385fabcaac1811d47eed2
Jacob Burenstam <burenstam@gmail.com>
2017-04-05 22:41:39 +0200

    JsonApiHelpers: Add support for Rails 5 error details and add meta data to model error-objects

--- lib/json_api_helpers/lib/json_api_helpers/serializers/model_error.rb
+++ lib/json_api_helpers/lib/json_api_helpers/serializers/model_error.rb
@@ -3,16 +3,30 @@ module JsonApiHelpers
   module Serializers
     module ModelError
       def self.serialize(model, key_transform: JsonApiHelpers.default_key_transform)
-        model.errors.messages.flat_map do |field, errors|
-          attribute_name = KeyTransform.call(field.to_s, key_transform: key_transform)
-          errors.map do |error_message|
+        model.errors.details.flat_map do |field, errors|
+          errors.map do |error|
+            attribute_name = KeyTransform.call(field.to_s, key_transform: key_transform)
+
             {
               status: 422,
               source: { pointer: "/data/attributes/#{attribute_name}" },
-              detail: error_message
+              detail: error_message_for(model.errors, field, error),
+              meta: error_meta_for(error)
             }
           end
         end
+      end
+
+      def self.error_message_for(model_errors, field, error)
+        model_errors.generate_message(
+          field, error.fetch(:error), count: error[:count]
+        )
+      end
+
+      def self.error_meta_for(error)
+        meta = { type: error.fetch(:error) }
+        meta[:count] = error[:count] if error[:count]
+        meta
       end
     end
   end
