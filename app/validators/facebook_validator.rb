00e9b96a60d8b21c269c2da03b9f1ca920a22609
Jacob Burenstam <burenstam@gmail.com>
2017-04-21 15:19:12 +0200

    Add facebook validator & improve linkedin validator

--- app/validators/facebook_validator.rb
+++ app/validators/facebook_validator.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
-class LinkedinValidator < ActiveModel::EachValidator
+class FacebookValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     value_changed = record.public_send("#{attribute}_changed?")
     return unless value_changed
     return if value.blank?
-    return if valid_linkedin_url?(value)
+    return if valid_facebook_url?(value)
 
-    message = options.fetch(:message) { I18n.t('errors.validators.linkedin_url') }
+    message = options.fetch(:message) { I18n.t('errors.validators.facebook_url') }
     record.errors.add(attribute, message)
   end
 
-  def valid_linkedin_url?(url)
+  def valid_facebook_url?(url)
     uri = URI.parse(url)
     return false unless uri.absolute?
-    return false unless uri.host.include?('linkedin.com')
+    return false unless uri.host.include?('facebook.com')
 
     true
   rescue URI::InvalidURIError => _e
