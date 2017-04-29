b28556f227fc2785c0877e9c2dd2bb2298fbd3a7
Jacob Burenstam <burenstam@gmail.com>
2017-04-29 14:05:57 +0200

    Code :lipstick:

--- app/validators/swedish_drivers_license_validator.rb
+++ app/validators/swedish_drivers_license_validator.rb
@@ -7,7 +7,7 @@ class SwedishDriversLicenseValidator < ActiveModel::EachValidator
     value_changed = record.public_send("#{attribute}_changed?")
     return unless value_changed
     return if value.blank?
-    
+
     return if Arbetsformedlingen::DriversLicenseCode.valid?(value)
 
     message = options.fetch(:message) { I18n.t('errors.validators.drivers_license') }
