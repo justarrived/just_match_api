d8c6185ef72cc9099ef932feaa0829ab22df6c2e
Jacob Burenstam <burenstam@gmail.com>
2017-04-30 19:02:19 +0200

    Add FrilansFinansApiLogSweeper that delets old entries

---
+++ spec/models/sweepers/frilans_finans_api_log_sweeper_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe Sweepers::FrilansFinansApiLogSweeper do
+  describe '#destroy_old' do
+    it 'destorys all old FrilansFinansApiLogs' do
+      expect(FrilansFinansApiLog.count).to eq(0)
+      log = FactoryGirl.create(:frilans_finans_api_log, created_at: 1.day.ago)
+      FactoryGirl.create(:frilans_finans_api_log, created_at: 1.year.ago)
+
+      described_class.destroy_old
+
+      expect(FrilansFinansApiLog.all).to eq([log])
+    end
+  end
+end
