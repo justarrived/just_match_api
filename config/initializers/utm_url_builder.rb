1af54658a527f868b75c625150bd886303ecd2c0
Jacob Burenstam <burenstam@gmail.com>
2017-05-10 21:46:57 +0200

    Add UtmUrlBuilder class and add utm param support to FrontendRouter

---
+++ config/initializers/utm_url_builder.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require 'utm_url_builder'
+
+UtmUrlBuilder.default_utm_source = 'JustMatch'
--- lib/frontend_routes.rb
+++ lib/frontend_routes.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
-class FrontendRoutesReader
+
+require 'utm_url_builder'
+
+class FrontendRouter
   attr_reader :routes, :base_url
+
+  def self.draw(name, **args)
+    new.draw(name, **args)
+  end
 
   def initialize
     @routes = YAML.load_file('config/frontend_routes.yml')['routes']
@@ -8,8 +15,21 @@ class FrontendRoutesReader
   end
 
   def draw(name, **args)
-    base_url + routes.fetch(name.to_s) % args
+    url = base_url + routes.fetch(name.to_s) % args
+    url_with_utm_params(url, args)
+  end
+
+  def url_with_utm_params(url, params)
+    utm_source = UtmUrlBuilder.default_utm_source || params[:utm_source]
+    return url unless utm_source
+
+    UtmUrlBuilder.build(
+      url,
+      source: utm_source,
+      medium: params[:utm_medium],
+      campaign: params[:utm_campaign],
+      term: params[:utm_term],
+      content: params[:utm_content]
+    )
   end
 end
-
-FrontendRouter = FrontendRoutesReader.new
