539aff4b4545d970d6fca7cf331050ec310b603a
Jacob Burenstam <burenstam@gmail.com>
2017-05-11 17:30:22 +0200

    Fix Dev::UserSeed

--- lib/seeds/dev/user_seed.rb
+++ lib/seeds/dev/user_seed.rb
@@ -7,7 +7,7 @@ module Dev
       max_users = max_count_opt('MAX_USERS', 50)
       max_company_users = max_count_opt('MAX_COMPANY_USERS', 5)
 
-      Faker::FrilansFinansApi.config.locale = 'sv'
+      Faker::Config.locale = 'sv'
 
       system_languages = languages.system_languages
 
