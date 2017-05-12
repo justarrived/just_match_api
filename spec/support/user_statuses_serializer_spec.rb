43854e58d0d2196f2f55d18ba888bde7705c289c
Jacob Burenstam <burenstam@gmail.com>
2017-05-12 16:08:30 +0200

    Fix broken test

--- spec/support/user_statuses_serializer_spec.rb
+++ spec/support/user_statuses_serializer_spec.rb
@@ -7,14 +7,15 @@ RSpec.describe UserStatusesSerializer do
     subject { described_class.serializeble_resource(key_transform: :dash).to_h }
 
     it 'returns serialized statues' do
-      data = subject[:data].first
-      expect(data[:id]).to eq(:asylum_seeker)
+      I18n.with_locale(:ar) do
+        data = subject[:data].first
+        expect(data[:id]).to eq(:asylum_seeker)
 
-      en_name = I18n.t('user.statuses.asylum_seeker', locale: :en)
-      ar_name = I18n.t('user.statuses.asylum_seeker', locale: :ar)
+        ar_name = I18n.t('user.statuses.asylum_seeker', locale: :ar)
 
-      expect(data[:attributes][:name]).to eq(en_name)
-      expect(data[:attributes][:'translated-text'][:name]).to eq(ar_name)
+        expect(data[:attributes][:name]).to eq(ar_name)
+        expect(data[:attributes][:'translated-text'][:name]).to eq(ar_name)
+      end
     end
   end
 end
