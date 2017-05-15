a17741705ae2a357820c739dbe042ed6c4beb205
Jacob Burenstam <burenstam@gmail.com>
2017-05-15 14:30:47 +0200

    Extract Linkedin::JobWrapper class from LinkedinJobsSerializer

--- app/serializers/linkedin_jobs_serializer.rb
+++ app/serializers/linkedin_jobs_serializer.rb
@@ -12,37 +12,25 @@ class LinkedinJobsSerializer
     builder.source do |source_node|
       source_node.publisherUrl('https://justarrived.se')
       source_node.publisher('Just Arrived')
-      jobs.each { |job| build_job_xml(source_node, job) }
+      jobs.each { |job| build_job_xml(source_node, Linkedin::JobWrapper.new(job: job)) }
     end
     builder.target!
   end
 
   def self.build_job_xml(parent_node, job)
     parent_node.job do |node|
-      node.company { |n| n.cdata!(job.company.name) }
+      node.company { |n| n.cdata!(job.company_name) }
 
-      node.partnerJobId { |n| n.cdata!(job.to_param) }
-      node.title { |n| n.cdata!(job.name.to_s) }
-      node.description do |n|
-        n.cdata!("#{job.description}\n\n#{special_linkedin_hashtag}")
-      end
+      node.partnerJobId { |n| n.cdata!(job.partner_job_id) }
+      node.title { |n| n.cdata!(job.title) }
+      node.description { |n| n.cdata!(job.description) }
 
-      node.location(job.full_street_address)
-      node.city { |n| n.cdata!(job.city.to_s) }
+      node.location(job.location)
+      node.city { |n| n.cdata!(job.city) }
       node.countryCode { |n| n.cdata!(job.country_code) }
-      node.postalCode { |n| n.cdata!(job.zip.to_s) }
+      node.postalCode { |n| n.cdata!(job.postal_code) }
 
-      node.applyUrl do |n|
-        n.cdata!(
-          FrontendRouter.draw(
-            :job,
-            id: job.to_param,
-            utm_source: 'linkedin',
-            utm_medium: 'ad',
-            utm_campaign: 'welcometalent'
-          )
-        )
-      end
+      node.applyUrl { |n| n.cdata!(job.apply_url) }
     end
   end
 
