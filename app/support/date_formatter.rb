0509786540b986c565b00857f12e5775b3cfac82
Jacob Burenstam <burenstam@gmail.com>
2017-05-29 21:04:36 +0200

    Update DateFormatter test and return nil when passed nil

--- app/support/date_formatter.rb
+++ app/support/date_formatter.rb
@@ -4,6 +4,6 @@ class DateFormatter
   include ActionView::Helpers::DateHelper
 
   def distance_of_time_in_words_from_now(datetime)
-    distance_of_time_in_words(Time.zone.now, datetime)
+    distance_of_time_in_words(Time.zone.now, datetime) if datetime
   end
 end
--- spec/support/date_formatter_spec.rb
+++ spec/support/date_formatter_spec.rb
@@ -3,17 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe DateFormatter do
-  describe '#force_utf8' do
-    it 'replaces all non-UTF-8 characters' do
-      expect(described_class.new.force_utf8("Watman \xBF")).to eq('Watman �')
-    end
-
-    it 'returns string untouched if chars are UTF-8' do
-      expect(described_class.new.force_utf8('WATMAN')).to eq('WATMAN')
+  describe '#distance_of_time_in_words_from_now' do
+    it 'returns the time from now' do
+      date = Time.zone.now + 5.days
+      result = described_class.new.distance_of_time_in_words_from_now(date)
+      expect(result).to eq('5 days')
     end
 
     it 'returns nil when passed nil' do
-      expect(described_class.new.force_utf8(nil)).to eq(nil)
+      expect(described_class.new.distance_of_time_in_words_from_now(nil)).to eq(nil)
     end
   end
 end
