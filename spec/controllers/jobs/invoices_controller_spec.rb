257494f9e52c7af94eb32dbc71f0ead855af9916
Jacob Burenstam <burenstam@gmail.com>
2017-06-07 16:38:56 +0200

    Don't pass header keyword to job invoices controller tests

--- spec/controllers/jobs/invoices_controller_spec.rb
+++ spec/controllers/jobs/invoices_controller_spec.rb
@@ -6,50 +6,39 @@ RSpec.describe Api::V1::Jobs::InvoicesController, type: :controller do
   let(:job_user) { FactoryGirl.create(:job_user_passed_job) }
   let(:user) { job_user.user }
   let(:job) { job_user.job }
-  let(:logged_in_user) do
-    user = job.owner
-    user.create_auth_token
-    user
-  end
+  let(:logged_in_user) { job.owner.tap(&:create_auth_token) }
   let(:valid_params) do
     {
+      auth_token: logged_in_user.auth_token,
       job_id: job.to_param,
       job_user_id: job_user.to_param
     }
   end
 
-  let(:valid_session) do
-    allow_any_instance_of(described_class).
-      to(receive(:current_user).
-      and_return(logged_in_user))
-
-    { token: logged_in_user.auth_token }
-  end
-
   describe 'POST #create' do
     context 'with valid params' do
       it 'returns 201 created' do
-        post :create, params: valid_params, headers: valid_session
+        post :create, params: valid_params
         expect(response.status).to eq(201)
       end
 
       it 'creates an Invoice' do
         expect do
-          post :create, params: valid_params, headers: valid_session
+          post :create, params: valid_params
         end.to change(Invoice, :count).by(1)
       end
 
       it 'notifies user' do
         allow(InvoiceCreatedNotifier).to receive(:call).
           with(job: job, user: user)
-        post :create, params: valid_params, headers: valid_session
+        post :create, params: valid_params
         expect(InvoiceCreatedNotifier).to have_received(:call)
       end
 
       context 'already created' do
         it 'returns 422 unprocessable entity' do
           FactoryGirl.create(:invoice, job_user: job_user)
-          post :create, params: valid_params, headers: valid_session
+          post :create, params: valid_params
           expect(response.status).to eq(422)
         end
 
@@ -57,7 +46,7 @@ RSpec.describe Api::V1::Jobs::InvoicesController, type: :controller do
           allow(InvoiceCreatedNotifier).to receive(:call).
             with(job: job, user: user)
           FactoryGirl.create(:invoice, job_user: job_user)
-          post :create, params: valid_params, headers: valid_session
+          post :create, params: valid_params
           expect(InvoiceCreatedNotifier).not_to have_received(:call)
         end
       end
