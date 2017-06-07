1f090cb1a9f3527722dfab0707c542e5b06ccddb
Jacob Burenstam <burenstam@gmail.com>
2017-06-07 15:23:21 +0200

    Don't pass header keyword to user chats controller tests

--- spec/controllers/users/user_chats_controller_spec.rb
+++ spec/controllers/users/user_chats_controller_spec.rb
@@ -3,23 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::Users::UserChatsController, type: :controller do
-  let(:user) { FactoryGirl.create(:user) }
-  let(:valid_session) do
-    allow_any_instance_of(described_class).
-      to(receive(:current_user).
-      and_return(user))
-    {}
-  end
+  let(:user) { FactoryGirl.create(:user_with_tokens) }
 
   describe 'GET #index' do
     context 'with valid params' do
       let(:valid_params) do
-        { user_id: user.to_param }
+        {
+          auth_token: user.auth_token,
+          user_id: user.to_param
+        }
       end
 
       it 'assigns all chats as @chats' do
         chat = FactoryGirl.create(:chat, users: [user])
-        get :index, params: valid_params, headers: valid_session
+        get :index, params: valid_params
         expect(assigns(:chats)).to eq([chat])
       end
     end
@@ -29,11 +26,15 @@ RSpec.describe Api::V1::Users::UserChatsController, type: :controller do
     context 'with valid params' do
       let(:chat) { FactoryGirl.create(:chat, users: [user]) }
       let(:valid_params) do
-        { user_id: user.to_param, id: chat.to_param }
+        {
+          auth_token: user.auth_token,
+          user_id: user.to_param,
+          id: chat.to_param
+        }
       end
 
       it 'assigns chat as @chat' do
-        get :show, params: valid_params, headers: valid_session
+        get :show, params: valid_params
         expect(assigns(:chat)).to eq(chat)
       end
     end
@@ -42,13 +43,13 @@ RSpec.describe Api::V1::Users::UserChatsController, type: :controller do
   describe 'GET #support_chat' do
     context 'with valid params' do
       let(:valid_params) do
-        { user_id: user.to_param }
+        { auth_token: user.auth_token, user_id: user.to_param }
       end
 
       it 'assigns chat as @chat' do
         admin_user = FactoryGirl.create(:admin_user)
         chat = FactoryGirl.create(:chat, users: [user, admin_user])
-        get :support_chat, params: valid_params, headers: valid_session
+        get :support_chat, params: valid_params
         expect(assigns(:chat)).to eq(chat)
       end
     end
