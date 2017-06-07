1c85bd8414fd3a62410b9d566ae303b877711fe1
Jacob Burenstam <burenstam@gmail.com>
2017-06-07 16:16:35 +0200

    Don't pass header keyword to acceptances controller tests

--- spec/controllers/jobs/acceptances_controller_spec.rb
+++ spec/controllers/jobs/acceptances_controller_spec.rb
@@ -4,29 +4,20 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::Jobs::AcceptancesController, type: :controller do
   let(:new_attributes) { {} }
-
-  let(:valid_session) do
-    user = FactoryGirl.create(:user_with_tokens, company: FactoryGirl.create(:company))
-    allow_any_instance_of(described_class).
-      to(receive(:current_user).
-      and_return(user))
-    { token: user.auth_token }
-  end
-
-  let(:user) { User.find_by_auth_token(valid_session[:token]) }
-  # let(:owner) { User.find_by_auth_token(valid_session[:token]) }
+  let(:user) { FactoryGirl.create(:company_user).tap(&:create_auth_token) }
 
   it 'notifies user when updated JobUser#accepted is set to true' do
     job = FactoryGirl.create(:job_with_users, users_count: 1, owner: user)
     job_user = job.job_users.first
     params = {
+      auth_token: user.auth_token,
       job_id: job.to_param,
       job_user_id: job_user.to_param
     }.merge(new_attributes)
 
     notifier_args = { job_user: job_user, owner: job.owner }
     allow(ApplicantAcceptedNotifier).to receive(:call).with(notifier_args)
-    post :create, params: params, headers: valid_session
+    post :create, params: params
     expect(ApplicantAcceptedNotifier).to have_received(:call).with(notifier_args)
   end
 
@@ -35,11 +26,12 @@ RSpec.describe Api::V1::Jobs::AcceptancesController, type: :controller do
     job_user = job.job_users.first
 
     params = {
+      auth_token: user.auth_token,
       job_id: job.to_param,
       job_user_id: job_user.to_param
     }.merge(new_attributes)
 
-    post :create, params: params, headers: valid_session
+    post :create, params: params
     expect(assigns(:job_user).accepted).to eq(true)
   end
 
@@ -47,11 +39,12 @@ RSpec.describe Api::V1::Jobs::AcceptancesController, type: :controller do
     job = FactoryGirl.create(:job_with_users, users_count: 1, owner: user)
     job_user = job.job_users.first
     params = {
+      auth_token: user.auth_token,
       job_id: job.to_param,
       job_user_id: job_user.to_param
     }.merge(new_attributes)
 
-    post :create, params: params, headers: valid_session
+    post :create, params: params
     expect(response.status).to eq(200)
   end
 end
