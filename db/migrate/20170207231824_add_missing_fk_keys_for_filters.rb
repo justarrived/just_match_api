6e2fb10901c0c26e12437f2d2a2faf76098bf801
Jacob Burenstam <burenstam@gmail.com>
2017-06-11 17:28:46 +0200

    Add ActiveRecord migration version to migration files

--- db/migrate/20170207231824_add_missing_fk_keys_for_filters.rb
+++ db/migrate/20170207231824_add_missing_fk_keys_for_filters.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddMissingFkKeysForFilters < ActiveRecord::Migration
+class AddMissingFkKeysForFilters < ActiveRecord::Migration[4.2]
   def change
     add_foreign_key 'language_filters', 'languages', name: 'language_filters_language_id_fk' # rubocop:disable Metrics/LineLength
     add_foreign_key 'skill_filters', 'skills', name: 'skill_filters_skill_id_fk'
