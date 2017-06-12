ad38579ddc57339a5ca9ca4ebcbe61fd1a5910d5
Jacob Burenstam <burenstam@gmail.com>
2017-06-12 15:47:00 +0200

    Admin: Whitelist permitted params for CompanyTranslations

--- app/admin/company_translation.rb
+++ app/admin/company_translation.rb
@@ -1,16 +1,13 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register CompanyTranslation do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params do
+    %i(
+      locale
+      short_description
+      description
+      language_id
+      company_id
+    )
+  end
 end
