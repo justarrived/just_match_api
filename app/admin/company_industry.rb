38a892693a5746c24c2026b9095fbee61b90e385
Jacob Burenstam <burenstam@gmail.com>
2017-06-22 16:47:48 +0200

    Admin: Add Company & Job industry undet the correct menu item

--- app/admin/company_industry.rb
+++ app/admin/company_industry.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register CompanyIndustry do
+  menu parent: 'Jobs'
+
   permit_params do
     %i(company_id industry_id)
   end
--- app/admin/job_industry.rb
+++ app/admin/job_industry.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register JobIndustry do
+  menu parent: 'Jobs'
+
   permit_params do
     %i(job_id industry_id importance years_of_experience)
   end
