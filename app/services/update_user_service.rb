8e9b7397bfcdcc560c911212563c6b8fcc7988b4
Jacob Burenstam <burenstam@gmail.com>
2017-08-21 18:11:58 +0200

    Extract UpdateUserService from UsersController

---
+++ app/services/update_user_service.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+class UpdateUserService
+  def self.call(**args)
+    new(**args).call
+  end
+
+  attr_reader :user, :params, :old_email
+
+  def initialize(user:, params:, language_ids:, skill_ids:, interest_ids:)
+    @user = user
+    @params = params
+    @old_email = @user.email
+
+    @language_ids = language_ids
+    @skill_ids = skill_ids
+    @interest_ids = interest_ids
+  end
+
+  def call
+    return user unless user.update(params)
+
+    set_translations
+
+    user.reload
+
+    enqueue_email_updated_events unless user.email == old_email
+    set_user_traits
+    enqueue_ff_user_sync
+
+    user
+  end
+
+  def set_translations
+    user.set_translation(params).tap do |result|
+      ProcessTranslationJob.perform_later(
+        translation: result.translation,
+        changed: result.changed_fields
+      )
+    end
+  end
+
+  def enqueue_email_updated_events
+    JobDigestSubscriberSyncJob.perform_later(user: user)
+  end
+
+  def set_user_traits
+    SetUserTraitsService.call(
+      user: user,
+      language_ids_param: @language_ids,
+      skill_ids_param: @skill_ids,
+      interest_ids_param: @interest_ids
+    )
+  end
+
+  def enqueue_ff_user_sync
+    return unless AppConfig.frilans_finans_active?
+
+    SyncFrilansFinansUserJob.perform_later(user: user)
+  end
+end
