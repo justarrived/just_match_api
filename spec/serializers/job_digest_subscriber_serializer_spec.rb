350e7ec617166ccf36c629c0fff7b4965e19de83
Jacob Burenstam <burenstam@gmail.com>
2017-08-21 10:38:51 +0200

    Annotate spec serializers job digest subscriber

--- spec/serializers/job_digest_subscriber_serializer_spec.rb
+++ spec/serializers/job_digest_subscriber_serializer_spec.rb
@@ -32,11 +32,24 @@ end
 
 # == Schema Information
 #
-# Table name: job_digests
+# Table name: job_digest_subscribers
 #
-#  id                     :integer          not null, primary key
-#  city                   :string
-#  notification_frequency :integer
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
+#  id            :integer          not null, primary key
+#  email         :string
+#  uuid          :string(36)
+#  user_id       :integer
+#  job_digest_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+# Indexes
+#
+#  index_job_digest_subscribers_on_job_digest_id  (job_digest_id)
+#  index_job_digest_subscribers_on_user_id        (user_id)
+#  index_job_digest_subscribers_on_uuid           (uuid) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (job_digest_id => job_digests.id)
+#  fk_rails_...  (user_id => users.id)
 #
