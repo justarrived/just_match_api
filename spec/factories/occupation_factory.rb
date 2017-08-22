e4e4e4516f595ffc8f9087355c2356777e547c54
Jacob Burenstam <burenstam@gmail.com>
2017-08-22 19:48:29 +0200

    Spec update

--- spec/factories/occupation_factory.rb
+++ spec/factories/occupation_factory.rb
@@ -4,6 +4,13 @@ FactoryGirl.define do
   factory :occupation do
     name 'MyString'
     association :language
+
+    factory :occupation_for_docs do
+      id 1
+      name 'Carpenter'
+      created_at Time.new(2016, 2, 10, 1, 1, 1).utc
+      updated_at Time.new(2016, 2, 12, 1, 1, 1).utc
+    end
   end
 end
 
--- spec/routing/occupations_routing_spec.rb
+++ spec/routing/occupations_routing_spec.rb
@@ -1,4 +1,25 @@
 # frozen_string_literal: true
+# == Schema Information
+#
+# Table name: occupations
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  ancestry    :string
+#  language_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_occupations_on_ancestry     (ancestry)
+#  index_occupations_on_language_id  (language_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (language_id => languages.id)
+#
+
 
 require 'rails_helper'
 
