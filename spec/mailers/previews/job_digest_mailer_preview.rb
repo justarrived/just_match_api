76899c2fc89abbfc57a970d312ec20e5fafb0b42
Jacob Burenstam <burenstam@gmail.com>
2017-08-22 19:05:23 +0200

    Improved job digest HTML email styling

--- spec/mailers/previews/job_digest_mailer_preview.rb
+++ spec/mailers/previews/job_digest_mailer_preview.rb
@@ -5,7 +5,7 @@ class JobDigestMailerPreview < ActionMailer::Preview
   def digest_email
     JobDigestMailer.digest_email(
       email: 'watman@example.com',
-      jobs: Job.last(3),
+      jobs: Job.last(5),
       job_digest: JobDigest.new(job_digest_subscriber_id: 67)
     )
   end
