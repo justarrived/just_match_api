8a552113127570b36d495bd7cc0f043b0345d24f
Jacob Burenstam <burenstam@gmail.com>
2017-09-21 14:45:57 +0200

    API: Return job descripton as HTML in metrojobb partner feed

--- app/models/metrojobb/job_wrapper.rb
+++ app/models/metrojobb/job_wrapper.rb
@@ -8,7 +8,6 @@ module Metrojobb
       @job = job
     end
 
-    delegate :description, to: :job
     delegate :city, to: :job
     delegate :full_time, to: :job
 
@@ -26,6 +25,10 @@ module Metrojobb
 
     def summary
       job.short_description
+    end
+
+    def description
+      StringFormatter.to_html(job.description)
     end
 
     def employer
