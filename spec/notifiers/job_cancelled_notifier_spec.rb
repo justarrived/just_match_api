b591271423f3403935e8b2f1b6a11e60f8b18f4d
Jacob Burenstam <burenstam@gmail.com>
2017-10-17 12:57:03 +0200

    Don't send job cancelled notifications to users that have withdrawn their application. Closes #1221

--- spec/notifiers/job_cancelled_notifier_spec.rb
+++ spec/notifiers/job_cancelled_notifier_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe JobCancelledNotifier, type: :mailer do
   let(:mailer) { Struct.new(:deliver_later).new(nil) }
   let(:job) { FactoryGirl.create(:job_with_users) }
 
-  it 'must work' do
+  it 'sends notification to user' do
     user = job.users.first
     allow(JobMailer).to receive(:job_cancelled_email).
       and_return(mailer)
@@ -15,5 +15,17 @@ RSpec.describe JobCancelledNotifier, type: :mailer do
 
     mailer_args = { user: user, job: job }
     expect(JobMailer).to have_received(:job_cancelled_email).with(mailer_args)
+  end
+
+  it 'does not send notification to a user who has withdrawn their application' do
+    job = FactoryGirl.create(:job)
+    FactoryGirl.create(:job_user, job: job, application_withdrawn: true)
+
+    allow(JobMailer).to receive(:job_cancelled_email).
+      and_return(mailer)
+
+    described_class.call(job: job)
+
+    expect(JobMailer).not_to have_received(:job_cancelled_email)
   end
 end
