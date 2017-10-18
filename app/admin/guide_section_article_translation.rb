62ab5e55ef1c91b479c2d20e1fd187749aa91f12
Jacob Burenstam <burenstam@gmail.com>
2017-10-18 11:51:16 +0200

    Admin: Custom GuideSectionArticleTranslation form

--- app/admin/guide_section_article_translation.rb
+++ app/admin/guide_section_article_translation.rb
@@ -35,6 +35,20 @@ ActiveAdmin.register GuideSectionArticleTranslation do
     end
   end
 
+  form do |f|
+    f.inputs 'Article translation' do
+      f.input :article
+      f.input :language, collection: Language.system_languages
+      f.input :locale, collection: I18n.available_locales
+      f.input :title
+      f.input :slug
+      f.input :short_description
+      f.input :body, as: :text, input_html: { markdown: true }
+    end
+
+    f.actions
+  end
+
   permit_params do
     %i[
       title
