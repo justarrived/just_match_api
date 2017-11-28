23b1694ce7cb8e35b39a72354bc35983176f55af
Jacob Burenstam <burenstam@gmail.com>
2017-11-28 21:45:05 +0100

    Admin: Add permit params to UtalkCode

--- app/admin/utalk_code.rb
+++ app/admin/utalk_code.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register UtalkCode do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params do
+    %i[
+      code
+      user_id
+      claimed_at
+    ]
+  end
+
+  controller do
+    def scoped_collection
+      super.includes(:user)
+    end
+  end
 end
