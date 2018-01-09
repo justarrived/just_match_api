00c37c0e15a96f2a8dbe821e98dd5ce2f44fbb8c
Jacob Burenstam <burenstam@gmail.com>
2018-01-09 11:49:00 +0100

    Bump rails 5.1.4 => 5.2.0.beta2

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130082854) do
+ActiveRecord::Schema.define(version: 2017_11_30_082854) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
   enable_extension "hstore"
   enable_extension "pg_stat_statements"
+  enable_extension "plpgsql"
   enable_extension "unaccent"
 
   create_table "active_admin_comments", id: :serial, force: :cascade do |t|
