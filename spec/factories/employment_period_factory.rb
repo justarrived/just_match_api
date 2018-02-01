f90600a30d1e48b113148df5577d0aa2c3c88160
Jacob Burenstam <burenstam@gmail.com>
2018-02-01 21:03:22 +0100

    Annotate EmploymentPeriod files

--- spec/factories/employment_period_factory.rb
+++ spec/factories/employment_period_factory.rb
@@ -10,3 +10,28 @@ FactoryBot.define do
     ended_at '2018-02-01 20:58:53'
   end
 end
+
+# == Schema Information
+#
+# Table name: employment_periods
+#
+#  id                 :integer          not null, primary key
+#  job_user_id        :integer
+#  user_id            :integer
+#  employer_signed_at :datetime
+#  employee_signed_at :datetime
+#  started_at         :datetime
+#  ended_at           :datetime
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+# Indexes
+#
+#  index_employment_periods_on_job_user_id  (job_user_id)
+#  index_employment_periods_on_user_id      (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (job_user_id => job_users.id)
+#  fk_rails_...  (user_id => users.id)
+#
--- spec/models/employment_period_spec.rb
+++ spec/models/employment_period_spec.rb
@@ -5,3 +5,28 @@ require 'rails_helper'
 RSpec.describe EmploymentPeriod, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+# == Schema Information
+#
+# Table name: employment_periods
+#
+#  id                 :integer          not null, primary key
+#  job_user_id        :integer
+#  user_id            :integer
+#  employer_signed_at :datetime
+#  employee_signed_at :datetime
+#  started_at         :datetime
+#  ended_at           :datetime
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+# Indexes
+#
+#  index_employment_periods_on_job_user_id  (job_user_id)
+#  index_employment_periods_on_user_id      (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (job_user_id => job_users.id)
+#  fk_rails_...  (user_id => users.id)
+#
