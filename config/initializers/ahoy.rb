a97ba80e32e9533afbe3e1eac2a2f77a0148b3ec
Jacob Burenstam <burenstam@gmail.com>
2018-03-19 22:30:49 +0100

    Update Ahoy initializer config to be compliant with v2

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
-module Ahoy
-  class Store < Ahoy::Stores::ActiveRecordTokenStore
-    def user
-      return if controller.nil?
-      return controller.true_user if controller.respond_to?(:true_user)
-      controller.current_user if controller.respond_to?(:current_user)
-    end
+class Ahoy::Store < Ahoy::DatabaseStore
+  def visit_model
+    Visit
+  end
+
+  def user
+    return if controller.nil?
+    return controller.true_user if controller.respond_to?(:true_user)
+    controller.current_user if controller.respond_to?(:current_user)
   end
 end
 
-Ahoy.api_only = true
+Ahoy.api = true
+Ahoy.server_side_visits = :when_needed
 Ahoy.geocode = :async
