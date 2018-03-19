23468b63f77bf9c4c957aec4a37e1e25628e8d40
Jacob Burenstam <burenstam@gmail.com>
2018-03-19 22:32:35 +0100

    Fix: Use nested module/class definitions instead of compact style

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
-class Ahoy::Store < Ahoy::DatabaseStore
-  def visit_model
-    Visit
-  end
+module Ahoy
+  class Store < Ahoy::DatabaseStore
+    def visit_model
+      Visit
+    end
 
-  def user
-    return if controller.nil?
-    return controller.true_user if controller.respond_to?(:true_user)
-    controller.current_user if controller.respond_to?(:current_user)
+    def user
+      return if controller.nil?
+      return controller.true_user if controller.respond_to?(:true_user)
+      controller.current_user if controller.respond_to?(:current_user)
+    end
   end
 end
 
