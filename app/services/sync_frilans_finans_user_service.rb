62539f5c7a2d35b1faf6e6285435ab18ddfaac22
Jacob Burenstam <burenstam@gmail.com>
2018-03-30 13:08:22 +0200

    Move update of user#frilans_finans_id to sync service

--- app/services/sync_frilans_finans_user_service.rb
+++ app/services/sync_frilans_finans_user_service.rb
@@ -2,18 +2,18 @@
 
 class SyncFrilansFinansUserService
   def self.call(user:)
-    id = user.frilans_finans_id
     attributes = FrilansFinans::UserWrapper.attributes(user)
 
-    if id
-      FrilansFinansAPI::User.update(id: id, attributes: attributes)
-    else
-      ff_user = FrilansFinansAPI::User.create(attributes: attributes)
-      ff_id = ff_user.resource.id
-      return if ff_id.nil?
+    if id = user.frilans_finans_id
+      return FrilansFinansAPI::User.update(id: id, attributes: attributes)
+    end
 
-      user.frilans_finans_id = ff_id
-      user.save!
+    FrilansFinansAPI::User.create(attributes: attributes).tap do |document|
+      ff_id = document.resource.id
+      if ff_id
+        user.frilans_finans_id = ff_id
+        user.save!
+      end
     end
   end
 end
