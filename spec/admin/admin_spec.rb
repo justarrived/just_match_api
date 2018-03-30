d1092a645e4b62708f32fff3b6314c430ea6941e
Jacob Burenstam <burenstam@gmail.com>
2018-03-30 16:55:37 +0200

    Update admin spec to remove some deprecation warnings

--- spec/admin/admin_spec.rb
+++ spec/admin/admin_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe 'Admin' do
           request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(user.email, password) # rubocop:disable Metrics/LineLength
           get :index
         end
-        it { expect(subject).to be_success }
+        it { expect(subject).to be_successful }
       end
 
       show_ignores = %(Comment Ratings)
@@ -49,7 +49,7 @@ RSpec.describe 'Admin' do
             resource = FactoryBot.create(model_name.constantize.model_name.singular)
             get :show, params: { id: resource.id }
           end
-          it { expect(subject).to be_success }
+          it { expect(subject).to be_successful }
         end
       end
     end
