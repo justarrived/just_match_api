4b9438f9ff6583c69028f0d6220188a986815206
Jacob Burenstam <burenstam@gmail.com>
2018-08-28 08:01:55 +0200

    Fix JobUser#current_status for non Frilans Finans jobs

--- app/models/job_user.rb
+++ app/models/job_user.rb
@@ -86,7 +86,7 @@ class JobUser < ApplicationRecord
   end
 
   def current_status
-    if job.started?
+    if job.started? && job.frilans_finans_job?
       return 'Not signed by user!' if accepted && !will_perform
 
       ff_status = frilans_finans_invoice&.ff_payment_status_name
